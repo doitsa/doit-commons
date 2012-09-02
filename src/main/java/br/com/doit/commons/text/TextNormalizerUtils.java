ad7aae1d71131ad0dbb4c3371a0a0c62b39d1f8d
Henrique Prange <hprange@gmail.com>
2012-09-02 19:40:21 -0300

    Adiciona classe que faz tratamento de textos

---
+++ src/main/java/br/com/doit/commons/text/TextNormalizerUtils.java
@@ -0,0 +1,95 @@
+package br.com.doit.commons.text;
+
+import java.text.Normalizer;
+import java.text.Normalizer.Form;
+
+/**
+ * A classe <code>TextNormalizerUtils</code> contém funções auxiliares para facilitar o tratamento/normalização de
+ * textos (<code>String</code>). Todas as funções auxiliares são capazes de tratar um parâmetro nulo sem gerar uma
+ * exceção.
+ * 
+ * @author <a href="mailto:hprange@gmail.com.br">Henrique Prange</a>
+ */
+public class TextNormalizerUtils {
+    /**
+     * Remove caracteres não alfanuméricos do texto sem estragar a formatação do texto (espaços, pontos, dois-pontos e
+     * hífen são mantidos).
+     * 
+     * @param text
+     *            O texto que deverá ser tratado
+     * @return Retorna o texto tratado
+     */
+    public static String lightStripNonAlphanumericChars(String text) {
+        if (text == null) {
+            return null;
+        }
+
+        return toAscii(text).replaceAll("[^a-zA-Z0-9\\s\\.:-]", "").replaceAll("\n", "").trim();
+    }
+
+    /**
+     * Remove todos os caracteres não alfanuméricos do texto.
+     * 
+     * @param text
+     *            O texto que deverá ser tratado
+     * @return Retorna o texto contendo apenas caracteres alfanuméricos
+     */
+    public static String stripNonAlphanumericChars(String text) {
+        if (text == null) {
+            return null;
+        }
+
+        return toAscii(text).replaceAll("[^a-zA-Z0-9\\s]", "").replaceAll("\n", "").trim();
+    }
+
+    /**
+     * Remove todos os caracteres não numéricos do texto.
+     * 
+     * @param text
+     *            O texto que deverá ser tratado
+     * @return Retorna o texto contendo apenas caracteres numéricos
+     */
+    public static String stripNonNumericChars(String text) {
+        if (text == null) {
+            return null;
+        }
+
+        return text.replaceAll("[^\\d]", "");
+    }
+
+    /**
+     * Converte todos os caracteres para a representação ASCII.
+     * 
+     * @param text
+     *            O texto que deverá ser tratado
+     * @return Retorna o texto contendo apenas caracteres ASCII
+     */
+    public static String toAscii(final String text) {
+        if (text == null) {
+            return null;
+        }
+
+        return Normalizer.normalize(text, Form.NFD).replaceAll("[^\\p{ASCII}]", "");
+    }
+
+    /**
+     * Remove espaços em branco antes e depois do texto.
+     * 
+     * @param text
+     *            O texto que deverá ser tratado
+     * @return Retorna uma cópia do texto sem espações antes ou depois do texto
+     */
+    public static String trim(String text) {
+        if (text == null) {
+            return null;
+        }
+
+        return text.trim();
+    }
+
+    /**
+     * Não deve ser instanciada.
+     */
+    private TextNormalizerUtils() {
+    }
+}
---
+++ src/test/java/br/com/doit/commons/text/TestTextNormalizerUtils.java
@@ -0,0 +1,108 @@
+package br.com.doit.commons.text;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+/**
+ * @author <a href="mailto:hprange@gmail.com">Henrique Prange</a>
+ */
+public class TestTextNormalizerUtils {
+    @Test
+    public void doNotStripBlankSpaceFromTheMiddleOfWords() throws Exception {
+        String result = TextNormalizerUtils.stripNonAlphanumericChars("abc def");
+
+        assertThat(result, is("abc def"));
+    }
+
+    @Test
+    public void normalizeNullString() throws Exception {
+        String result = TextNormalizerUtils.toAscii(null);
+
+        assertThat(result, nullValue());
+    }
+
+    @Test
+    public void normalizeStringWithSpecialChars() throws Exception {
+        String result = TextNormalizerUtils
+                .toAscii("\u00e1\u00e9\u00ed\u00f3\u00fa\u00c1\u00c9\u00cd\u00d3\u00da\u00e7\u00c7\u00e3\u00f5");
+
+        assertThat(result, is("aeiouAEIOUcCao"));
+    }
+
+    @Test
+    public void removeAllLineBreaks() {
+        String result = TextNormalizerUtils
+                .stripNonAlphanumericChars("Rua \nGuaip\u00e1,\n 1443 , /");
+
+        assertThat(result, is("Rua Guaipa 1443"));
+    }
+
+    @Test
+    public void stripNonAlphanumericCharsAndConvertSpecialChars()
+            throws Exception {
+        String result = TextNormalizerUtils.stripNonAlphanumericChars("123',\u00e1\u00e9\u00ed\u00f3\u00fa.~456");
+
+        assertThat(result, is("123aeiou456"));
+    }
+
+    @Test
+    public void stripNonAlphanumericCharsFromNullString() throws Exception {
+        String result = TextNormalizerUtils.stripNonAlphanumericChars(null);
+
+        assertThat(result, nullValue());
+    }
+
+    @Test
+    public void stripNonAlphanumericCharsFromString() throws Exception {
+        String result = TextNormalizerUtils
+                .stripNonAlphanumericChars("prefix123',.~456sufix");
+
+        assertThat(result, is("prefix123456sufix"));
+    }
+
+    @Test
+    public void stripNonNumericCharsFromNullString() throws Exception {
+        String result = TextNormalizerUtils.stripNonNumericChars(null);
+
+        assertThat(result, nullValue());
+    }
+
+    @Test
+    public void stripNonNumericCharsFromString() throws Exception {
+        String result = TextNormalizerUtils.stripNonNumericChars("123.456.789-09ABC");
+
+        assertThat(result, is("12345678909"));
+    }
+
+    @Test
+    public void trimAfterRemovingSpecials() {
+        String result = TextNormalizerUtils
+                .stripNonAlphanumericChars("Rua Guaip\u00e1, 1443 , /");
+
+        assertThat(result, is("Rua Guaipa 1443"));
+    }
+
+    @Test
+    public void trimNullString() throws Exception {
+        String result = TextNormalizerUtils.trim(null);
+
+        assertThat(result, nullValue());
+    }
+
+    @Test
+    public void trimStrippedString() throws Exception {
+        String result = TextNormalizerUtils.stripNonAlphanumericChars("   abc   ");
+
+        assertThat(result, is("abc"));
+    }
+
+    @Test
+    public void trimTextRemovesBlanksBeforeAndAfter() throws Exception {
+        String result = TextNormalizerUtils.trim("   abc   \n\n ");
+
+        assertThat(result, is("abc"));
+    }
+}
