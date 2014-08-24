96b9128ea2f70b0172d53957de12b6a643f4ada5
Elizio Marcelino <elizio.marcelino@gmail.com>
2014-08-24 18:23:54 -0300

    Criando novos Modes e ajustando o código para retornar o resto de um cálculo.

--- src/main/java/br/com/doit/commons/math/MathUtils.java
+++ src/main/java/br/com/doit/commons/math/MathUtils.java
@@ -1,12 +1,18 @@
 package br.com.doit.commons.math;
 
-import static br.com.doit.commons.math.MathUtils.Mode.IGNORING_QUANTITY;
+import static br.com.doit.commons.math.MathUtils.RemainderDistributionMode.IGNORING_QUANTITY;
+import static br.com.doit.commons.math.MathUtils.RemainderDistributionMode.STRICTLY_PROPORTIONAL;
+import static br.com.doit.commons.math.MathUtils.RemainderDistributionMode.UNEVENLY;
 import static java.math.BigDecimal.ZERO;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -17,25 +23,6 @@ import java.util.Map;
  */
 public class MathUtils {
     /**
-     * Os modos de distribuição do resto, caso a distribuição pró-rata não seja exata.
-     * 
-     * @author <a href="mailto:hprange@gmail.com.br">Henrique Prange</a>
-     */
-    public enum Mode {
-        /**
-         * Ignora a quantidade de cada <code>Fraction</code> na hora de distribuir o resto. Nesse modo, é garantido que
-         * todo o valor passado por parâmetro será distribuídos entre as <code>Fraction</code>s.
-         */
-        IGNORING_QUANTITY,
-
-        /**
-         * Ignora o resto caso não seja possível distribuir o valor de forma proporcional, levando em conta a quantidade
-         * de cada <code>Fraction</code>.
-         */
-        IGNORING_REMAINDER
-    }
-
-    /**
      * Essa classe será usada em conjunto com a operação de distribuição de um valor. Ela não deve ser usada de forma
      * isolada.
      * 
@@ -43,7 +30,8 @@ public class MathUtils {
      */
     public static final class Preparation {
         private static boolean isRemainderDivisibleByQuantity(BigDecimal remainder, BigDecimal quantity) {
-            if (ZERO.compareTo(quantity) == 0) {
+
+            if (ZERO.compareTo(remainder) == 0 || ZERO.compareTo(quantity) == 0) {
                 return false;
             }
 
@@ -51,9 +39,9 @@ public class MathUtils {
         }
 
         private final BigDecimal amount;
-        private final Mode mode;
+        private final RemainderDistributionMode mode;
 
-        private Preparation(BigDecimal amount, Mode mode) {
+        private Preparation(BigDecimal amount, RemainderDistributionMode mode) {
             this.amount = amount;
             this.mode = mode;
         }
@@ -68,12 +56,20 @@ public class MathUtils {
          * 
          * @param whole
          *            Uma coleção de <code>Fraction</code>s que representam o todo
+         * 
+         * @return Caso esteja utilizando o <code>Mode</code> do tipo <code>RETURNING_REMAINDER</code> será retornado o
+         *         valor do resto.
+         *         Caso esteja utilizando o <code>Mode</code> do tipo <code>IGNORING_QUANTITY</code> será feito a
+         *         distribuição do resto, assim será retornado <code>BigDecimal.ZERO</code>.
          */
-        public void over(Collection<Fraction> whole) {
+        public BigDecimal over(Collection<Fraction> whole) {
+
+            whole = sortedByValue(whole);
+
             BigDecimal total = total(whole);
 
             if (ZERO.compareTo(total) == 0) {
-                return;
+                return ZERO;
             }
 
             BigDecimal totalShare = ZERO;
@@ -95,7 +91,9 @@ public class MathUtils {
             for (Fraction fraction : whole) {
                 BigDecimal share = shares.get(fraction);
 
-                if (isRemainderDivisibleByQuantity(remainder, fraction.quantity()) || mode == IGNORING_QUANTITY) {
+                if (mode != STRICTLY_PROPORTIONAL
+                        &&
+                        (isRemainderDivisibleByQuantity(remainder, fraction.quantity()) || mode == IGNORING_QUANTITY)) {
                     BigDecimal subtotal = fraction.value().multiply(fraction.quantity());
 
                     BigDecimal available = subtotal.subtract(share);
@@ -113,6 +111,19 @@ public class MathUtils {
 
                 fraction.setShare(share);
             }
+
+            return remainder;
+        }
+
+        private Collection<Fraction> sortedByValue(Collection<Fraction> whole) {
+            List<Fraction> listSortable = new ArrayList<Fraction>(whole);
+            Collections.sort(listSortable, Collections.reverseOrder(new Comparator<Fraction>() {
+                @Override
+                public int compare(Fraction o1, Fraction o2) {
+                    return o1.value().compareTo(o2.value());
+                }
+            }));
+            return listSortable;
         }
 
         private BigDecimal total(Collection<Fraction> whole) {
@@ -127,6 +138,31 @@ public class MathUtils {
     }
 
     /**
+     * Os modos de distribuição do resto, caso a distribuição pró-rata não seja exata.
+     * 
+     * @author <a href="mailto:hprange@gmail.com.br">Henrique Prange</a>
+     */
+    public enum RemainderDistributionMode {
+        /**
+         * Ignora a quantidade de cada <code>Fraction</code> na hora de distribuir o resto. Nesse modo, é garantido que
+         * todo o valor passado por parâmetro será distribuídos entre as <code>Fraction</code>s.
+         */
+        IGNORING_QUANTITY,
+
+        /**
+         * Retorna o valor do resto e ignora a distribuição do mesmo caso não seja possível distribuir o valor de forma
+         * proporcional.
+         */
+        STRICTLY_PROPORTIONAL,
+
+        /**
+         * Ignora o resto caso não seja possível distribuir o valor de forma proporcional, levando em conta a quantidade
+         * de cada <code>Fraction</code>.
+         */
+        UNEVENLY
+    }
+
+    /**
      * Distribui um valor de forma proporcional entre o conjunto de <code>Fraction</code>s que formam o todo. Ignora o
      * resto caso a divisão proporcional não seja exata. Exemplo de uso:
      * 
@@ -138,7 +174,7 @@ public class MathUtils {
      *            o valor que será distribuído entre as <code>Fraction</code>s que compõem o todo
      */
     public static Preparation distribute(BigDecimal amount) {
-        return new Preparation(amount, Mode.IGNORING_REMAINDER);
+        return new Preparation(amount, UNEVENLY);
     }
 
     /**
@@ -154,7 +190,7 @@ public class MathUtils {
      * @param mode
      *            define o que deve ser feito com o resto caso a divisão proporcional não seja exata
      */
-    public static Preparation distribute(BigDecimal amount, Mode mode) {
+    public static Preparation distribute(BigDecimal amount, RemainderDistributionMode mode) {
         return new Preparation(amount, mode);
     }
 
--- src/test/java/br/com/doit/commons/math/TestProRataAllocationUtils.java
+++ src/test/java/br/com/doit/commons/math/TestProRataAllocationUtils.java
@@ -1,11 +1,14 @@
 package br.com.doit.commons.math;
 
-import static br.com.doit.commons.math.MathUtils.Mode.IGNORING_QUANTITY;
+import static br.com.doit.commons.math.MathUtils.RemainderDistributionMode.IGNORING_QUANTITY;
+import static br.com.doit.commons.math.MathUtils.RemainderDistributionMode.STRICTLY_PROPORTIONAL;
 import static java.math.BigDecimal.ONE;
+import static java.math.BigDecimal.TEN;
 import static java.math.BigDecimal.ZERO;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -15,7 +18,6 @@ import java.util.Collection;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 /**
  * @author <a href="mailto:hprange@gmail.com.br">Henrique Prange</a>
@@ -35,12 +37,37 @@ public class TestProRataAllocationUtils {
     }
 
     @Test
+    public void distributeAmountAndReturnRemainderWhenModeIsReturningRemainder() throws Exception {
+        Fraction fraction = addFraction(new BigDecimal("2"), ONE);
+        Fraction fraction2 = addFraction(new BigDecimal("2"), ONE);
+
+        BigDecimal remainder = MathUtils.distribute(new BigDecimal("0.10"), STRICTLY_PROPORTIONAL).over(
+                whole);
+
+        verify(fraction).setShare(new BigDecimal("0.04"));
+        verify(fraction2).setShare(new BigDecimal("0.04"));
+        assertThat(remainder, is(new BigDecimal("0.02")));
+    }
+
+    @Test
     public void distributeAmountEqualToZeroWhenAtLeastOneFraction() throws Exception {
         Fraction fraction = addFraction(ONE, ONE);
 
-        MathUtils.distribute(ZERO).over(whole);
+        BigDecimal remainder = MathUtils.distribute(ZERO).over(whole);
 
         verify(fraction).setShare(new BigDecimal("0.00"));
+        assertThat(remainder, is(new BigDecimal("0.00")));
+    }
+
+    @Test
+    public void distributeAmountWhenIsDivisibleEqual() throws Exception {
+        Fraction fraction = addFraction(new BigDecimal("2"), ONE);
+        Fraction fraction2 = addFraction(new BigDecimal("2"), ONE);
+        BigDecimal remainder = MathUtils.distribute(TEN).over(whole);
+
+        verify(fraction).setShare(new BigDecimal("5.00"));
+        verify(fraction2).setShare(new BigDecimal("5.00"));
+        assertThat(remainder, is(new BigDecimal("0.00")));
     }
 
     @Test
@@ -56,9 +83,10 @@ public class TestProRataAllocationUtils {
     public void distributeAmountWhenOneFraction() throws Exception {
         Fraction fraction = addFraction(ONE, ONE);
 
-        MathUtils.distribute(BigDecimal.ONE).over(whole);
+        BigDecimal remainder = MathUtils.distribute(BigDecimal.ONE).over(whole);
 
         verify(fraction).setShare(new BigDecimal("1.00"));
+        assertThat(remainder, is(new BigDecimal("0.00")));
     }
 
     @Test
@@ -66,19 +94,11 @@ public class TestProRataAllocationUtils {
         Fraction fraction = addFraction(ZERO, ONE);
         Fraction fraction2 = addFraction(ONE, ONE);
 
-        MathUtils.distribute(BigDecimal.ONE).over(whole);
+        BigDecimal remainder = MathUtils.distribute(BigDecimal.ONE).over(whole);
 
         verify(fraction).setShare(new BigDecimal("0.00"));
         verify(fraction2).setShare(new BigDecimal("1.00"));
-    }
-
-    @Test
-    public void distributeAmountWhenTotalOfFractionsIsZero() throws Exception {
-        Fraction fraction = addFraction(ZERO, ZERO);
-
-        MathUtils.distribute(ONE).over(whole);
-
-        verify(fraction, never()).setShare(Mockito.any(BigDecimal.class));
+        assertThat(remainder, is(new BigDecimal("0.00")));
     }
 
     @Test
@@ -86,10 +106,11 @@ public class TestProRataAllocationUtils {
         Fraction fraction = addFraction(new BigDecimal("2"), new BigDecimal("10"));
         Fraction fraction2 = addFraction(new BigDecimal("8"), new BigDecimal("10"));
 
-        MathUtils.distribute(new BigDecimal("10")).over(whole);
+        BigDecimal remainder = MathUtils.distribute(new BigDecimal("10")).over(whole);
 
         verify(fraction).setShare(new BigDecimal("2.00"));
         verify(fraction2).setShare(new BigDecimal("8.00"));
+        assertThat(remainder, is(new BigDecimal("0.00")));
     }
 
     @Test
@@ -97,10 +118,11 @@ public class TestProRataAllocationUtils {
         Fraction fraction = addFraction(ONE, ONE);
         Fraction fraction2 = addFraction(ONE, ONE);
 
-        MathUtils.distribute(BigDecimal.ONE).over(whole);
+        BigDecimal remainder = MathUtils.distribute(BigDecimal.ONE).over(whole);
 
         verify(fraction).setShare(new BigDecimal("0.50"));
         verify(fraction2).setShare(new BigDecimal("0.50"));
+        assertThat(remainder, is(new BigDecimal("0.00")));
     }
 
     @Test
@@ -108,10 +130,11 @@ public class TestProRataAllocationUtils {
         Fraction fraction = addFraction(ONE, ZERO);
         Fraction fraction2 = addFraction(ONE, ONE);
 
-        MathUtils.distribute(BigDecimal.ONE).over(whole);
+        BigDecimal remainder = MathUtils.distribute(BigDecimal.ONE).over(whole);
 
         verify(fraction).setShare(new BigDecimal("0.00"));
         verify(fraction2).setShare(new BigDecimal("1.00"));
+        assertThat(remainder, is(new BigDecimal("0.00")));
     }
 
     @Test
@@ -119,10 +142,11 @@ public class TestProRataAllocationUtils {
         Fraction fraction = addFraction(new BigDecimal("13"), new BigDecimal("100000"));
         Fraction fraction2 = addFraction(new BigDecimal("13"), new BigDecimal("100000"));
 
-        MathUtils.distribute(new BigDecimal("0.36"), IGNORING_QUANTITY).over(whole);
+        BigDecimal remainder = MathUtils.distribute(new BigDecimal("0.36"), IGNORING_QUANTITY).over(whole);
 
         verify(fraction).setShare(new BigDecimal("0.23"));
         verify(fraction2).setShare(new BigDecimal("0.13"));
+        assertThat(remainder, is(ZERO));
     }
 
     @Test
@@ -130,10 +154,11 @@ public class TestProRataAllocationUtils {
         Fraction fraction = addFraction(ONE, ONE);
         Fraction fraction2 = addFraction(new BigDecimal("2"), ONE);
 
-        MathUtils.distribute(new BigDecimal("3")).over(whole);
+        BigDecimal remainder = MathUtils.distribute(new BigDecimal("3")).over(whole);
 
         verify(fraction).setShare(new BigDecimal("1.00"));
         verify(fraction2).setShare(new BigDecimal("2.00"));
+        assertThat(remainder, is(new BigDecimal("0.00")));
     }
 
     @Test
@@ -141,10 +166,11 @@ public class TestProRataAllocationUtils {
         Fraction fraction = addFraction(new BigDecimal("12"), new BigDecimal("100000"));
         Fraction fraction2 = addFraction(new BigDecimal("12"), new BigDecimal("100000"));
 
-        MathUtils.distribute(new BigDecimal("0.36")).over(whole);
+        BigDecimal remainder = MathUtils.distribute(new BigDecimal("0.36")).over(whole);
 
         verify(fraction).setShare(new BigDecimal("0.24"));
         verify(fraction2).setShare(new BigDecimal("0.12"));
+        assertThat(remainder, is(ZERO));
     }
 
     @Test
@@ -154,27 +180,39 @@ public class TestProRataAllocationUtils {
         Fraction fraction3 = addFraction(ONE, new BigDecimal("0.01"));
         Fraction fraction4 = addFraction(ONE, new BigDecimal("39.89"));
 
-        MathUtils.distribute(new BigDecimal("70")).over(whole);
+        BigDecimal remainder = MathUtils.distribute(new BigDecimal("70")).over(whole);
 
-        verify(fraction).setShare(new BigDecimal("0.01"));
-        verify(fraction2).setShare(new BigDecimal("35.00"));
+        verify(fraction).setShare(new BigDecimal("0.00"));
+        verify(fraction2).setShare(new BigDecimal("35.01"));
         verify(fraction3).setShare(new BigDecimal("0.00"));
         verify(fraction4).setShare(new BigDecimal("34.99"));
+        assertThat(remainder, is(ZERO));
     }
 
     @Test
-    public void ignoreRemainderWhenFractionHasNoDivisableQuantity() throws Exception {
+    public void returningRemainderWhenFractionHasNoDivisableQuantity() throws Exception {
         Fraction fraction = addFraction(new BigDecimal("13"), new BigDecimal("100000"));
         Fraction fraction2 = addFraction(new BigDecimal("13"), new BigDecimal("100000"));
 
-        MathUtils.distribute(new BigDecimal("0.36")).over(whole);
+        BigDecimal remainder = MathUtils.distribute(new BigDecimal("0.36")).over(whole);
 
         verify(fraction).setShare(new BigDecimal("0.13"));
         verify(fraction2).setShare(new BigDecimal("0.13"));
+        assertThat(remainder, is(new BigDecimal("0.10")));
+    }
+
+    @Test
+    public void returnZeroWhenTotalOfFractionsIsZero() throws Exception {
+        addFraction(ZERO, ZERO);
+
+        BigDecimal remainder = MathUtils.distribute(ONE).over(whole);
+
+        assertThat(remainder, is(ZERO));
     }
 
     @Before
     public void setup() {
         whole = new ArrayList<Fraction>();
     }
+
 }
