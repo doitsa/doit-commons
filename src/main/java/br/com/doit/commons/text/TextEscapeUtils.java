fd3e20e59135f85037c89a8c86148c7118e1a890
Henrique Prange <hprange@gmail.com>
2018-08-28 11:55:11 -0300

    Corrige alguns typos na documentação

--- src/main/java/br/com/doit/commons/text/TextEscapeUtils.java
+++ src/main/java/br/com/doit/commons/text/TextEscapeUtils.java
@@ -10,13 +10,11 @@ import static org.apache.commons.lang.StringUtils.replace;
 public class TextEscapeUtils {
 
     /**
-     * Troca quebra de linhas de uam String de System.lineSeparator() para a tag <br/>
-     * ,
-     * dessa forma lugares que exibem o endereco interpretando HTML podem exibir as quebras de linhas
+     * Troca quebra de linhas de uma {@code String} de System.lineSeparator() para a tag <br/>.
+     * Dessa forma, lugares que exibem o endereço interpretando HTML podem exibir as quebras de linha.
      *
-     * @param string
-     * @return Uma String com a tag <br/>
-     *         ao invés de System.lineSeparator()
+     * @param string A {@code String} que terá as quebras de linha substituídas.
+     * @return Uma String com a tag <br/> ao invés de System.lineSeparator().
      */
     public static String escapeHtmlLineBreaks(String string) {
         return replace(string, "\n", "<br>");
