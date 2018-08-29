8523d3f48cdbcb9734edfad00a31e47a66eb8187
Luiz Alfredo <luizalfredocb@gmail.com>
2018-08-29 19:58:34 -0300

    Adequa método

--- src/main/java/br/com/doit/commons/text/TextEscapeUtils.java
+++ src/main/java/br/com/doit/commons/text/TextEscapeUtils.java
@@ -10,14 +10,16 @@ import static org.apache.commons.lang.StringUtils.replace;
 public class TextEscapeUtils {
 
     /**
-     * Troca quebra de linhas de uma {@code String} de System.lineSeparator() para a tag <br/>.
-     * Dessa forma, lugares que exibem o endereço interpretando HTML podem exibir as quebras de linha.
+     * Troca quebra de linhas de uam String de System.lineSeparator() para a tag <br/>
+     * ,
+     * dessa forma lugares que exibem o endereco interpretando HTML podem exibir as quebras de linhas
      *
-     * @param string A {@code String} que terá as quebras de linha substituídas.
-     * @return Uma String com a tag <br/> ao invés de System.lineSeparator().
+     * @param string
+     * @return Uma String com a tag <br/>
+     *         ao invés de System.lineSeparator()
      */
     public static String escapeHtmlLineBreaks(String string) {
-        return replace(string, "\n", "<br>");
+        return replace(string, System.lineSeparator(), "<br>");
     }
 
 }
